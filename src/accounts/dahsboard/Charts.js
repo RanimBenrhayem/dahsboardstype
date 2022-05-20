54f9ea8d90152e0913c079091d75419d93c64419
RanimBenrhayem <benrhayemranim6@gmail.com>
2022-05-20 18:42:48 +0200

    dashtype

--- src/accounts/dahsboard/Charts.js
+++ src/accounts/dahsboard/Charts.js
@@ -13,7 +13,7 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { Bar, Bubble, Line ,Pie} from 'react-chartjs-2';
+import { Bar, Bubble, Line ,Pie, Radar,Doughnut,Scatter, PolarArea} from 'react-chartjs-2';
 import axios from "axios";
 import ReactToPrint, { PrintContextConsumer } from "react-to-print";
 import { LineChart } from "recharts";
@@ -44,7 +44,8 @@ const Charts =   ()=> {
     const [attribut1, setAttribut1] = useState("");
     const [attribut2, setAttribut2] = useState("");
     const [graph,setGraph] = useState({}) ;
-    const [show,setShow] = useState(false)
+    const [show,setShow] = useState(false);
+    const[showAlert,setShowAlert] = useState(true)
     const ref = useRef(null);
     function getHeadersFromCsv(data) {
         return data.slice(0, data.indexOf("\n")).split(",");
@@ -85,6 +86,7 @@ const Charts =   ()=> {
         try{
             if (fileToDraw.length > 0) {
                 setFileData([]);
+               
                 const response =await axios({
                     method :"post",
                     url : `http://localhost:8080/chart/draw/${fileToDraw}`,
@@ -103,10 +105,13 @@ const {xaxis,yaxis,labels,returnedData} = response.data
                             data: values,
                             backgroundColor: [
                                 'rgb(255, 99, 132)',
-                                'rgb(54, 162, 235)',
+                                'rgb(60, 179, 113)',
                                 'rgb(255, 205, 86)',
-                                  'rgb(255, 99, 71)',
-                                  'rgb(60, 179, 113)'
+                                'rgb(54, 162, 235)',
+                                'rgb(255, 255, 0)',
+                               
+                               
+                                 
                               ],
                               hoverOffset: 8
 
@@ -117,136 +122,188 @@ const {xaxis,yaxis,labels,returnedData} = response.data
                     ],
                 })
             }
+            
             setShow(true)
+            
         }catch (e) {
             console.log(e)
         }
-
     }
+
+    const saveInfoDashboard = async ()=> {
+        try {
+            
+
+            if (fileToDraw.length > 0) {
+                setFileData([]);
+                const response =await axios({
+                    method :"post",
+                    url : `http://localhost:8080/chart/save/database`,
+                    data : {xaxis:attribut1 , yaxis : attribut2,fileToDraw : `${fileToDraw}`}
+                })}
+
+
+
+
+
+
+
+        } catch (error) {
+            
+        }
+    }
+    const downloadImage = useCallback (()=>{
+        const link = document.createElement('a');
+        link.download = 'chart.png'
+        link.href = ref.current.toBase64Image()
+        link.click();
+    },[])
+        
+
+
+
+
 /******************* chart js options ***********************/
 
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Your Bar Chart',
-            },
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
         },
-    };
-function barCharts(){
-   
-    return(
-     <h1>hhhhhhh</h1>
-    )
-}
-const downloadImage = useCallback (()=>{
-    const link = document.createElement('a');
-    link.download = 'chart.png'
-    link.href = ref.current.toBase64Image()
-    link.click();
-},[])
-    return (
-        <>
-        <h2>Bar chart</h2>
-       
-        <button onClick={barCharts}>Add Bar charts</button>
-        
-<div className="boxdashboard">
-                <select value={fileToDraw} onChange={handleFileOptions}>
-                    <option value={""}>__please choose a file__</option>
-                    {files.map((element) => {
-                        return (
-                            <option value={element._id}>
-                                {element.metadata.originalFileName}
-                            </option>
-                        );
-                    })}
-                </select>
-            <div className="selectoptiondashboard">
-                {headers1.length > 0 && (
-                    <select
-                        value={attribut1}
-                        onChange={(e) => setAttribut1(e.target.value)}
-                    >
-                        <option value={""}>_please select an attribut_</option>
-                        {headers1.map((element, index) => {
-                            let subElt = element.substring(2, element.length - 2);
-                            if (index === 0) {
-                                subElt = subElt.substring(1);
-                            } else if (index === headers1.length - 1) {
-                                subElt = subElt.substring(0, subElt.length - 2);
-                            }
+        title: {
+            display: true,
+            text: 'Your  Chart',
+        },
+    },
+};
+  const [chart, setChart] = React.useState();
 
-                            return <option value={element}>{element}</option>;
-
-                        })}
-                    </select>
-
-                    )}
-
-            </div>
-    <div className="selectoptiondashboard">
-        {headers1.length > 0 && (
-            <select
-                value={attribut2}
-                onChange={(e) => setAttribut2(e.target.value)}
-            >
-                <option value={""}>_please select an attribut_</option>
-                {headers1.map((element, index) => {
-                    let subElt = element.substring(2, element.length - 2);
-                    if (index === 0) {
-                        subElt = subElt.substring(1);
-                    } else if (index === headers1.length - 1) {
-                        subElt = subElt.substring(0, subElt.length - 2);
-                    }
-
-                    return <option value={element}>{element}</option>;
-
-                })}
-            </select>
-
-        )}
-          {headers1.length > 0 && (
-              <> <button className="buttonShowDashbord" >SAVE</button>
-              <button className="buttonShowDashbord" onClick={handleProcess}>View</button></>
-        
-          )}
-    
-    
+  function Chart(e) {
+    if (e.value === "bar") {
+      return (
+        <div style={{height : '720px' , width : '750px',marginLeft : '-70px',marginTop:'-20px'}}>
+          
+          {show && <Bar  ref={ref} options={options} data={graph} />}
         </div>
+      );
+    } else if (e.value === "pie") {
+      return (
+        <div style={{height : '450px' , width : '450px',marginLeft : '20px',marginTop:'-70px'}}>
+          {show && <Pie ref={ref} options={options} data={graph}  />}
+        </div>
+      );
+    } else if (e.value === "bubble") {
+      return (
+        <div style={{height : '620px' , width : '650px',marginLeft : '-70px',marginTop:'-20px'}}>
+          {show && (
+            <Bubble ref={ref} options={options} data={graph}  />
+          )}
+          
+        </div>
+      );
+    } else if (e.value === "polararea") {
+        return (
+          <div style={{height : '450px' , width : '450px',marginLeft : '20px',marginTop:'-70px'}}>
+            {show && (
+              <PolarArea ref={ref} options={options} data={graph}  />
+            )}
+    </div>
+        )
+    
+  }}
 
-</div>
-<div  
-      style={{height : '690px' , width : '690px',marginLeft : '100px'}}>
-            {
-                
-                show && (
-                    <> 
-                   
-                    <button onClick={downloadImage}>download as image</button>
-                   < Pie ref={ref} options={options} data={graph}  />
-                  
-                   </>
-                   
-                )
-                
-            }
-           
-            
+  return (
+    <React.Fragment>
+      <div style={{ marginTop: -200, marginLeft: -300 }}>
+        <select value={fileToDraw} onChange={handleFileOptions} className="select1">
+          <option value={""}>__please choose a file__</option>
+          {files.map((element) => {
+            return (
+              <option value={element._id}>
+                {element.metadata.originalFileName}
+              </option>
+            );
+          })}
+        </select>
+        <div >
+          {headers1.length > 0 && (
+            <select className="select2"
+              value={attribut1}
+              onChange={(e) => setAttribut1(e.target.value)}
+            >
+              <option value={""}>_please select an attribut_</option>
+              {headers1.map((element, index) => {
+                let subElt = element.substring(2, element.length - 2);
+                if (index === 0) {
+                  subElt = subElt.substring(1);
+                } else if (index === headers1.length - 1) {
+                  subElt = subElt.substring(0, subElt.length - 2);
+                }
 
+                return <option value={element}>{element}</option>;
+              })}
+            </select>
+          )}
+        </div>
+        <div >
+          {headers1.length > 0 && (
+            <select className="select3"
+              value={attribut2}
+              onChange={(e) => setAttribut2(e.target.value)}
+            >
+              <option value={""}>_please select an attribut_</option>
+              {headers1.map((element, index) => {
+                let subElt = element.substring(2, element.length - 2);
+                if (index === 0) {
+                  subElt = subElt.substring(1);
+                } else if (index === headers1.length - 1) {
+                  subElt = subElt.substring(0, subElt.length - 2);
+                }
 
-</div>
+                return <option value={element}>{element}</option>;
+              })}
+            </select>
+          )}
+            {headers1.length > 0 && (
+                <>
+          <button className="buttonShow" onClick={handleProcess} >
+           View
+          </button>
+           <button className="buttonShow" onClick={handleProcess}>
+           Save
+          </button>
+          <button  className="buttondownloadasimage" onClick={downloadImage}>download as image</button>
+          </> )}
+        </div>
+        <div>
+          {headers1.length > 0 && (
+            <>
+              <select 
+               className="select4"
+                defaultValue="Select Chart"
+                onChange={(e) => setChart(e.target.value)}
+              >
+                   <option value="type">choose type of chart</option>
+                <option value="bar">Bar Chart</option>
+                <option value="pie">pie Chart</option>
+                <option value="bubble">Bubble Chart</option>
+                <option value="polararea">PolarArea Chart</option>
+              </select>
 
+              <h1 className="selectechart">Selected Chart: {chart}</h1>
+              <Chart  value={chart} />
+            </>
+          )}
+        </div>
+      </div>
+      {/* <div 
+      style={{height : '400px' , width : '400px',marginLeft : '450px',marginTop:'100px'}}>
+        {show && <Pie options={options} data={graph}  />}
+      </div> */}
+    </React.Fragment>
+  );
+};
 
-        </>
-    )
-
-}
-
-
-export default Charts ;
+export default Charts;
--- src/accounts/dahsboard/Dashboard.js
+++ src/accounts/dahsboard/Dashboard.js
@@ -1,12 +1,33 @@
-import React from "react";
+import {
+  Grid,
+  Box,
+  Container,
+  Typography,
+  Toolbar,
+  AppBar,
+  IconButton,
+  TextField,
+  Paper,
+  Button,
+  Tooltip,
+} from "@mui/material";
+import React, { useState } from "react";
 import LayoutHome from "../layout/LayoutHome";
 import Charts from "./Charts";
 
 export default function DashboardHome() {
+  const[list,setList] =useState(1)
   return (
     <div>
+    
       <LayoutHome />
-        <Charts/>
+      <h2>Create your own Dashboards</h2>
+      <Paper sx={{ padding: "2em 50em", marginTop:8,
+          height: 500, width : 1000 }}elevation={5}>
+       {[...Array(list)].map((element)=><Charts />)}
+       <button className="addnewchart" onClick={()=>setList(list+1)}>add new chart</button>
+  </Paper>
+
     </div>
   );
 }
